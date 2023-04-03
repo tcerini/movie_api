a010cce7ab4bac96e59c6b522c6bb6b3f93eec79
tcerini <tommycerini@gmail.com>
2023-04-03 15:11:12 +0200

    morgan logging added

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 
+//have expresss use common morgan setup
+app.use(morgan('common'));
+
+//morgan log writing
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+app.use(morgan('combined', {stream: accessLogStream}));
+
