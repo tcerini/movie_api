106a90fb04e473ae5d6ef4712549c24561521418
tcerini <tommycerini@gmail.com>
2023-04-03 15:11:23 +0200

    movie array added

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const app = express();
 //have expresss use common morgan setup
 app.use(morgan('common'));
 
+//top movie array for json
+let topMovies = [
+    {
+        title: 'There Will Be Blood'
+    }
+];
+
 //morgan log writing
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 app.use(morgan('combined', {stream: accessLogStream}));
