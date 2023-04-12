8f6ed050ae47cbf46089ad975872671cda130019
tcerini <tommycerini@gmail.com>
2023-04-12 17:18:45 +0200

    CRUD functions added

--- index.js
+++ index.js
@@ -302,13 +302,123 @@ app.use(morgan('combined', {stream: accessLogStream}));
 
 //welcome message
 app.get("/", (req, res) => {
-    console.log("Welcome to myFlix");
-    res.send("Welcome to myFlix!");
-  });
+  console.log("Welcome to myFlix");
+  res.send("Welcome to myFlix!");
+});
 
-//have /movies URL call topMpovies
+//POST (create) user data
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send("Users needs names")
+  }
+
+})
+
+//POST (create) user data for top movies
+app.post('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+ 
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favouriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to User ${id}'s array`)
+  } else {
+    res.status(400).send("Movie not added")
+  }
+})
+
+//DELETE user data for top movies
+app.delete('/users/:id/:movieTitle', (req, res) => {
+  const { id, movieTitle } = req.params;
+ 
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    user.favouriteMovies = user.favouriteMovies.filter( title => title !== movieTitle)
+    res.status(200).send(`${movieTitle} has been removed from User ${id}'s array`)
+  } else {
+    res.status(400).send("Movie not deleted")
+  }
+})
+
+//DELETE user
+app.delete('/users/:id', (req, res) => {
+  const { id, movieTitle } = req.params;
+ 
+  let user = users.find( user => user.id == id );
+
+  if (user) {
+    users = users.filter( user => user.id != id)
+    res.status(200).send(`User ${id} has been deleted`)
+  } else {
+    res.status(400).send("User not found")
+  }
+})
+
+//PUT (update) user data
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find( user => user.id == id );
+
+  if(user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user)
+  } else {
+    res.status(400).send("User ID doens't exist")
+  }
+})
+
+//GET (read) - URL /movies to return all topMpovies
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.status(200).json(topMovies);
+});
+
+//GET (read) - URL /movies/[title] to return movie data
+app.get('/movies/:title', (req, res) => {
+  const { title } = req.params;
+  const movie = topMovies.find( movie => movie.Title === title );
+  
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("This is not a Top Movie")
+  }
+
+});
+
+// GET (read) - URL /movies/genre/[genre name] to return genre information
+app.get('/movies/genre/:genreName', (req, res) => {
+  const { genreName } = req.params;
+  const genre = topMovies.find( movie => movie.Genre.Name === genreName ).Genre;
+  
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("Genre not in Top Movies")
+  }
+
+});
+
+// GET (read) - URL /movies/genre/[genre name] to return genre information
+app.get('/movies/directors/:directorName', (req, res) => {
+  const { directorName } = req.params;
+  const director = topMovies.find( movie => movie.Director.Name === directorName ).Director;
+  
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("Director not in Top Movies")
+  }
+
 });
 
 //serve documentation.html from public folder
