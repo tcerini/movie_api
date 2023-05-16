8bade2683b6e22a3d6310b4e0ca0789b5692032e
tcerini <tommycerini@gmail.com>
2023-05-16 18:19:07 +0200

    admin

--- passport.js
+++ passport.js
@@ -14,13 +14,20 @@ passport.use(new LocalStrategy({
   console.log(username + '  ' + password);
   Users.findOne({ Username: username })
     .then(user => {
+
       if (!user) {
         console.log('incorrect username');
-        return callback(null, false, { message: 'Incorrect username or password.' });
+        return callback(null, false, {message: 'Incorrect username.'});
+      }
+  
+      if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect password.'});
       }
 
       console.log('finished');
       return callback(null, user);
+
     })
     .catch(error => {
       console.log(error);
