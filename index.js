452279cf21bab0638198637bf5a64e20818797ae
tcerini <tommycerini@gmail.com>
2023-06-07 16:01:17 +0200

    added allowed localhost

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ require('./passport');
 //CORS Middleware (after ./auth) allowed origins)
 const cors = require('cors');
 let allowedOrigins = ['http://localhost:8080'];
+let allowedOrigins = ['http://localhost:1234'];
 
 app.use(cors({
   origin: (origin, callback) => {
