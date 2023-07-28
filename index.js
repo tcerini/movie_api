506981f74dfd9efe798a4bf2e9b9dd87ea6cc763
tcerini <tommycerini@gmail.com>
2023-07-28 13:15:59 +0200

    defined hashedPassword

--- index.js
+++ index.js
@@ -153,6 +153,7 @@ app.post('/users',
 
 //UPDATE (change) - users data (all) by 'name'
 app.put('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+	let hashedPassword = Users.hashPassword(req.body.Password);
 	Users.findOneAndUpdate(
 		{ Username: req.params.Username },
 		{
